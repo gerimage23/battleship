b48ef44760f11d5e7e38fa9ad47ba768d8d04e61
Gergely Németh <ngerle@gmail.com>
2017-03-28 21:29:40 +0200

    Add display module

--- battleship_main.py
+++ battleship_main.py
@@ -2,70 +2,7 @@ import copy
 import random
 from subprocess import call
 
-
-def clear():
-    call('clear')
-    print('''.______        ___   .___________.___________. __       _______     _______. __    __   __  .______      
-|   _  \      /   \  |           |           ||  |     |   ____|   /       ||  |  |  | |  | |   _  \     
-|  |_)  |    /  ^  \ `---|  |----`---|  |----`|  |     |  |__     |   (----`|  |__|  | |  | |  |_)  |    
-|   _  <    /  /_\  \    |  |        |  |     |  |     |   __|     \   \    |   __   | |  | |   ___/     
-|  |_)  |  /  _____  \   |  |        |  |     |  `----.|  |____.----)   |   |  |  |  | |  | |  |         
-|______/  /__/     \__\  |__|        |__|     |_______||_______|_______/    |__|  |__| |__| | _|         
-                                                                                                         ''')
-
-
-def print_board(board_you=None, board_enemy=None):
-
-    if board_you != None:
-        if player != "Computer":
-            print('\n' + player + "'s board")
-            print('\n   ', end='')
-            for i in range(1, 11):
-                print(i, end=' ')
-            print('')
-            for i, j in enumerate(board_you):
-                if i > 9:
-                    break
-                print("{0:2d} {1}".format(i + 1, " ".join(j)))
-
-    if board_enemy != None:
-        if player_2 == "Computer" and player != player_2:
-            print('\nComputer\'s board')
-        elif player != "Computer":
-            print('\nEnemy\'s board')
-        print('\n   ', end='')
-        for i in range(1, 11):
-            print(i, end=' ')
-        print('')
-        letters = [ship_color(ship[0]) for ship in ships.keys()]
-        for i, j in enumerate(board_enemy):
-            if i > 9:
-                break
-            j_hidden = copy.deepcopy(j)
-            for x in range(10):
-                if j_hidden[x] in letters:
-                    j_hidden[x] = "#"
-            print("{0:2d} {1}".format(i + 1, " ".join(j_hidden)))
-
-
-def red(string):
-    # Change the color of the string to red
-    char = "\033[91m" + string + "\033[00m"
-    return char
-
-
-def green(string):
-    # Change the color of the string to green
-    char = '\x1b[6;30;42m' + string + '\x1b[0m'
-    # char = "\033[92m" + string + "\033[00m"
-    return char
-
-
-def ship_color(string):
-    # Change the color of the ship
-    char = '\x1b[1;34;40m' + string + '\x1b[0m'
-    # char = "\033[92m" + string + "\033[00m"
-    return char
+from display import *
 
 
 def computer_place_ships(board, ships, ships_data):
