b8a758c307aa16f99fe3ce97a2ad511e2e93892d
Gergely Németh <ngerle@gmail.com>
2017-03-28 22:14:09 +0200

    Move functions to place_ship module

--- place_ship.py
+++ place_ship.py
@@ -1,6 +1,77 @@
 import random
 
 
+def validate(board, ship, x, y, ori):
+    # validate the ship can be placed at given coordinates
+    if ori == "v" and x + ship > 10:
+        return False
+    elif ori == "h" and y + ship > 10:
+        return False
+    else:
+        if ori == "v":
+            for i in range(ship):
+                if board[x + i][y] != "#":
+                    return False
+        elif ori == "h":
+            for i in range(ship):
+                if board[x][y + i] != "#":
+                    return False
+    return True
+
+
+def v_or_h():
+    # get ship orientation from user
+    while(True):
+        user_input = input("Vertical or horizontal (v,h) ? ")
+        if user_input == "v" or user_input == "h":
+            return user_input
+        else:
+            print("Invalid input. Please only enter v or h")
+
+
+def get_coor():
+    valid = False
+    while not valid:
+        user_input = input("Please enter coordinates (row,col) ? ")
+        try:
+            # see that user entered 2 values seprated by comma
+            coor = user_input.split(",")
+            if len(coor) != 2:
+                raise Exception("Invalid entry, too few/many coordinates.")
+
+            # check that 2 values are integers
+            coor[0] = int(coor[0]) - 1
+            coor[1] = int(coor[1]) - 1
+
+            # check that values of integers are between 1 and 10 for both
+            # coordinates
+            if coor[0] > 9 or coor[0] < 0 or coor[1] > 9 or coor[1] < 0:
+                raise Exception(
+                    "Invalid entry. Please use values between 1 to 10 only.")
+
+            # if everything is ok, return coordinates
+            return coor
+
+        except ValueError:
+            print("Try giving numbers.")
+        except Exception as e:
+            print(e)
+
+
+# Placing ships
+def place_ship(board, ship, s, ori, x, y):
+
+    # place ship based on orientation
+    if ori == "v":
+        for i in range(ship):
+            board[x + i][y] = s
+    elif ori == "h":
+        for i in range(ship):
+            board[x][y + i] = s
+
+    return board
+
+
 def computer_place_ships(board, ships, ships_data):
     for ship in ships.keys():
         valid = False
