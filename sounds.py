4fa1c59dcb57dcb0bdb338583a270e48030b2b07
Gergely Németh <ngerle@gmail.com>
2017-03-30 15:53:02 +0200

    Try other method to make sound without error

--- sounds.py
+++ sounds.py
@@ -1,8 +1,25 @@
-import vlc
+# import vlc
+from pygame import mixer
 
 
 def play_sound(file):
+    # mixer.pre_init(44100, -16, 2, 1024)
+    # mixer.init()
+    # mixer.music.load(file)
+    # mixer.music.play()
+
     vlc.MediaPlayer(file).play()
+
+    # instance = vlc.Instance()
+    # player = instance.media_player_new()
+
+    # # Load the media file
+    # media = instance.media_new(file)
+
+    # # Add the media to the player
+    # player.set_media(media)
+
+    # player.play()
 
 
 def intro_sound():
@@ -34,4 +51,4 @@ def computer_wins_sound():
 
 
 def player_wins_sound():
-    play_sound("sounds/player_win.wav")
+    play_sound("sounds/player_win")
